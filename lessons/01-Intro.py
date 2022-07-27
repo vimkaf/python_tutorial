d64c90b3d7dd72fcb83170af0d4d59ae3f10dcdb
Kay <vimkaf@gmail.com>
2022-07-27 10:59:01 +0100

    Update lesson 1

--- lessons/01-Intro.py
+++ lessons/01-Intro.py
@@ -1,5 +1,47 @@
+
+
 print ("This is python!!")
 
+#this is a single line comment
+
+"""
+This is a multiline comment
+"""
 #variables in python
+#naming rules for variables
+"""
+A variable cannot start with a number -> 3a = 1 
+A variable can contain a number -> age1
+A variable can start with _
+A variable cannot be a keyword or reserved word -> print = 3
+"""
 a  = 21
+age =  10
+
+print(age)
+
+#numbers and float - Python Number Datatypes can contain integers, floating point numbers and imaginary numbers
+cost = 100.25
+
+#strings - Strings are datatypes that contain a character or sequence of characters
+firstname = "John"
+lastname = "Doe"
+address = '7, Boulevard Avenue'
+gender = 'F' 
+
+print(firstname,lastname)
+
+#booleans - Boolean is a datatype that can only have true or false values
+status = True #1
+print(status)
+
+
+
+
+
+
+
+
+
+
 
