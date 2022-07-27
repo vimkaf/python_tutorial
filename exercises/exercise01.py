d43def4c875efc4abfe0e8fac02760eedcc46062
Kay <vimkaf@gmail.com>
2022-07-27 12:02:51 +0100

    Updated Exercise 1

--- exercises/exercise01.py
+++ exercises/exercise01.py
@@ -1,20 +1,8 @@
 """
-Write a python program to print your name
+1. Write a python program to print your name
+2. Write a program to calculate the Simple Interest for a principal
+S.I = Principal * Rate * Time / 100
+Ask the user for the inputs
 """
-print ("ejike")
 
 
-"""
-Write a program to print the following pattern:
-*
-* *
-* * *
-* * * *
-* * * * *
-"""
-print ("*")
-print ("**")
-print ("***")
-print ("****")
-print ("*****")
-print ("******")
