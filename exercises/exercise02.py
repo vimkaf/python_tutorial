67cd5ae9edf67b7b9ae07defc3282771137ea68a
Kay <vimkaf@gmail.com>
2022-07-30 18:55:49 +0100

    Added exercise 2

---
+++ exercises/exercise02.py
@@ -0,0 +1,58 @@
+"""
+Question 1
+Write a program that reverse the list below
+list1 = [100, 200, 300, 400, 500]
+
+Expected Output:
+[500, 400, 300, 200, 100]
+
+
+Question 2
+Write a program to add two lists index-wise.
+Create a new list that contains the 0th index item from both the list,
+then the 1st index item, and so on till the
+last element. any leftover items will get added at the end of the new list.
+
+list1 = ["P", "y", "t", "h"]
+list2 = ["o", "n"," ", "R", "ocks"]
+
+Expected Output:
+["P", "y", "t", "h", "o", "n", " ", "R", "ocks"]
+
+Question 3
+Write a program to add item 4000 after 3000 in the following Python List
+
+list1 = [10, 20, [300, 400, [2000, 3000], 500], 30, 40]
+
+Expected Output:
+[10, 20, [300, 400, [2000, 3000, 4000], 500], 30, 40]
+
+Question 4
+You have given a nested list. 
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+
+Write a program to extend it by adding the sublist ["h", "i", "j"] 
+in such a way that it will look like the following list.
+
+Expected Output:
+['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
+
+Question 5
+You have given a Python list.
+Write a program to find value 10 in the list, and if it is present,
+replace it with 100. Only update the first occurrence of an item.
+
+list1 = [5, 10, 15, 20, 25, 50, 10]
+
+Expected Output:
+[5, 100, 15, 20, 25, 50, 100]
+
+Question 6
+Given a Python list, write a program to remove all occurrences of item 20.
+list1 = [5, 20, 15, 20, 25, 50, 20]
+
+Expected Output:
+[5, 15, 25, 50]
+
+
+"""
