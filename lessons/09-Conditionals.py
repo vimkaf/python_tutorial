a2c7932838152d24f41ca099a165fdda627c78d8
Kay <vimkaf@gmail.com>
2022-08-03 13:15:40 +0100

    Added lesson 9 and 10

---
+++ lessons/09-Conditionals.py
@@ -0,0 +1,28 @@
+"""
+This statements return a boolean value
+"""
+
+print ( 5 > 3 )
+
+print( 5 < 3)
+
+print( 5 == 3)
+
+print ( 5 == 5)
+
+print(5 == "5")
+
+print( 5 != 3 )
+
+print( 5 <= 3 )
+
+print( 5 >= 3 )
+
+print(5 > 3 and 5 < 10)
+
+print(5 > 3 or 5 < 3)
+
+
+
+
+
