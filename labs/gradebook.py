041da911853d337cacef7bf33ba3f14eb130fa98
erapheal121 <erapheal131@gmail.com>
2022-08-03 13:49:12 +0100

    evaluation for score

---
+++ labs/gradebook.py
@@ -0,0 +1,18 @@
+score = int(input("Score"))
+if score >= 80 or score == 100:
+    print("You scored an A")  
+elif score >= 60 or score == 80:
+    print("You scored a B")
+elif score >= 50 or score == 59:
+    print("You scored a C")
+elif score >= 40 or score == 49:
+    print("You scored a D") 
+elif score >= 30 or score == 39:
+    print("You scored an E")
+elif score >= 0 or score == 29:
+    print("You be olodo F")
+
+print("score ",score)    
+
+
+         
